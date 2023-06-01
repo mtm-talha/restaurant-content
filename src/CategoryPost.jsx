4a207a886a5c77b5a6732565972dab6c542da59d
muhammad talha <talha.mahmood@team.panelist.com>
2023-06-01 17:26:54 +0500

    added new header picture

--- src/CategoryPost.jsx
+++ src/CategoryPost.jsx
@@ -1,15 +1,19 @@
 const categoryPosts = [
   {
-    media: "/assets/1.jpg",
+    media: "/assets/Atlantis_Dubai.png",
     category: "Asian",
   },
   {
-    media: "/assets/1.jpg",
+    media: "/assets/DIFC.png",
     category: "Russian",
   },
   {
-    media: "/assets/1.jpg",
+    media: "/assets/Palm_Jumeirah.png",
     category: "Mexian",
+  },
+  {
+    media: "/assets/DIFC.png",
+    category: "Thai",
   },
   {
     media: "/assets/1.jpg",
@@ -20,14 +24,14 @@ function CategoryPost() {
   return (
     <section className="category-box-container">
       <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-5">
-      {categoryPosts?.map((item, index) => ( 
-        <div className="col" key={index}>
-          <div className="category-box" >
-            <img src={item.media} alt="Category icon" />
-            <h2>{item.category}</h2>
+        {categoryPosts?.map((item, index) => (
+          <div className="col" key={index}>
+            <div className="category-box">
+              <img src={item.media} alt="Category icon" />
+              <h2>{item.category}</h2>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </section>
   );
